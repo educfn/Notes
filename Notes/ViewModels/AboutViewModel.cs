8126267b593cef5e77a5662cc19c8c28b10c72ac
Eduardo Cardoso Fernandes Neto <eduardo@verion.ind.br>
2025-06-22 19:46:22 -0300

    [feature] About viewmodel and About view

--- Notes/ViewModels/AboutViewModel.cs
+++ Notes/ViewModels/AboutViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Notes;
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
-public class AboutViewModel
+namespace Notes.ViewModels;
+
+internal class AboutViewModel
 {
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "This app is writen in XAML and C# with .NET MAUI.";
+    public ICommand ShowMoreInfoCommand { get; }
 
+    public AboutViewModel()
+    {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+    }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
 }
